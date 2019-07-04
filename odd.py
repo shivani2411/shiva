41da7264881382f47c9793bf40115bd9a004740d
shivani2411 <39051434+shivani2411@users.noreply.github.com>
2019-07-04 11:25:39 +0530

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,5 +1,5 @@
-s,l=map(int,input().split())
-for i in range(s+1,l):
-  if(i%2!=0):
-    print(i,end=" ")
+n = list(map(int, input().split()))
+for i in range(n[0]+1, n[-1]):
+  if i % 2 != 0:
+    print(i, end=" ")
     
