86f4e6b4ed0d8f6d18536a9bf48a0b31924c4e45
shivani2411 <39051434+shivani2411@users.noreply.github.com>
2019-07-12 12:02:07 +0530

    Create giv.py

---
+++ giv.py
@@ -0,0 +1,11 @@
+s=int(input())
+b=list(map(int,input().split()))
+p=0
+d=[]
+for i in range(0,s+1):
+    if(b.count(i)>1):
+      d.append(i)
+if(len(d)==0):
+    print("unique")
+e=sorted(d)
+print(' '.join(map(str,e)))
