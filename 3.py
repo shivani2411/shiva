6e35de8f47280687619b319c738f10886df6a421
shivani2411 <39051434+shivani2411@users.noreply.github.com>
2019-08-03 10:44:06 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,13 +1,15 @@
 c11,c21=map(str,input().split())
 yas1=0
 if len(c11)>len(c21):
-	 c11,c21=c21,c11
+  c11,c21=c21,c11
 p1=0
 while p1<len(c11):
-	yas1+=(ord(c21[p1])-ord(c11[p1]))
+  yas1+=(ord(c21[p1])-ord(c11[p1]))
   p1+=1
 for p1 in range(p1,len(c21)):
-	 yas1+=ord(c21[p1])-ord('a')+1
+  yas1+=ord(c21[p1])-ord('a')+1
 print(yas1)
+
+
 
 
