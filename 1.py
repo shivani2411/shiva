f65c92ef4f5ef83a8cf09c9d7d80be25cd22cd7b
shivani2411 <39051434+shivani2411@users.noreply.github.com>
2019-08-03 10:35:29 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,9 @@
 from itertools import combinations
-a,k=map(int,input().split())
-ab=len(str(a))
-1st=list(combinations(str(a),ab-k))
-1st=sorted(1st)
-print(*1st[0],sep='')
+Na,ka=map(int,input().split())
+a1=len(str(Na))
+lst=list(combinations(str(Na),a1-ka))
+lst=sorted(lst)
+print(*lst[0],sep='')
+
+
+
